cc00e60182e590e10754b192df3b5d7fec126a86
Mahmoudahmf <mahmoud.ahm910@gmail.com>
2021-07-17 19:55:13 +0200

    Adding starred-repos route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StarredReposComponent } from './pages/starred-repos/starred-repos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'starred-repos', component: StarredReposComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
