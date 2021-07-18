d795b848780e134f7760b491aea155755fa3e039
Mahmoudahmf <mahmoud.ahm910@gmail.com>
2021-07-18 21:56:29 +0200

    Added get starred api

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { StarredRepo } from '../models/starred-repo.model';
+const STARRED_REPOS_URL = 'https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc';
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getStarredRepos = (): Observable<{ items: StarredRepo[] }> => {
+    return this.http.get(STARRED_REPOS_URL) as Observable<{ items: StarredRepo[] }>;
+  }
 }
