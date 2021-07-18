8054f9a2f1d6f4c872b9d4af0af8a1cdc3cf12f8
Mahmoudahmf <mahmoud.ahm910@gmail.com>
2021-07-18 23:41:55 +0200

    update starred repos page to use paginated service, added loading container for UX enhancements

--- src/app/pages/starred-repos/starred-repos.component.ts
+++ src/app/pages/starred-repos/starred-repos.component.ts
@@ -1,4 +1,6 @@
+import { StarredRepo } from './../../models/starred-repo.model';
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from 'src/app/services/github.service';
 
 @Component({
   selector: 'app-starred-repos',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./starred-repos.component.scss']
 })
 export class StarredReposComponent implements OnInit {
+  starredRepos: StarredRepo[];
+  pageNo: number;
+  isLoading: boolean;
+  constructor(private githubService: GithubService) {
+    this.starredRepos = [];
+    this.pageNo = 1;
+    this.isLoading = false;
+  }
 
-  constructor() { }
 
+  getStarredRepos = (pageNo?: number) => {
+    this.isLoading = true;
+    this.githubService.getStarredRepos(pageNo).subscribe((response: { items: StarredRepo[] }) => {
+      this.isLoading = false
+      console.log(response);
+      this.starredRepos = [...this.starredRepos, ...response.items];
+    }, error => {
+      this.isLoading = false;
+    })
+  }
   ngOnInit(): void {
+    this.getStarredRepos();
+  }
+
+  onScroll = () => {
+    this.pageNo++;
+    this.getStarredRepos(this.pageNo);
   }
 
 }
