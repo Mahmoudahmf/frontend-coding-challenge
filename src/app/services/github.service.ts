03006c37d65522503885fa0560e340a5fddf9ede
Mahmoudahmf <mahmoud.ahm910@gmail.com>
2021-07-18 23:36:37 +0200

    update github service to use pagination

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 import { Observable } from 'rxjs';
 import { StarredRepo } from '../models/starred-repo.model';
-const STARRED_REPOS_URL = 'https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc';
+const STARRED_REPOS_URL = `https://api.github.com/search/repositories?q=created:>${moment().subtract(30, 'days').format('YYYY-MM-DD')}&sort=stars&order=desc`;
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +12,7 @@ export class GithubService {
   constructor(private http: HttpClient) {
   }
 
-  getStarredRepos = (): Observable<{ items: StarredRepo[] }> => {
-    return this.http.get(STARRED_REPOS_URL) as Observable<{ items: StarredRepo[] }>;
+  getStarredRepos = (pageNo = 1): Observable<{ items: StarredRepo[] }> => {
+    return this.http.get(`${STARRED_REPOS_URL}&page=${pageNo}`) as Observable<{ items: StarredRepo[] }>;
   }
 }
